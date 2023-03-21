55fd62753f25387cb0a3b6d6372f0f4fe27230f8
pawas goyal <pawas48ram@gmail.com>
2023-03-21 23:12:22 +0530

    added new ques

---
+++ 900 level problems/badboy.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n,m,x,y;
+    cin>>n>>m>>x>>y;
+   cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+
+    }
+}
